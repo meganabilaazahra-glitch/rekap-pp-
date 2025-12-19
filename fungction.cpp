0d5107c309789dea95e403cea7c12be2249c8883
meganabilaazahra-glitch <meganabilaazahra@gmail.com>
2025-12-19 19:16:30 +0700

    Add introduction function in fungction.cpp

---
+++ fungction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void perkenalan(){
+    cout<<"Hi, nama saya adalah mega nabila azahra dari kelas A"<<endl;
+    cout<<"Sahabat sejati saya adalah nela meli"<<endl;
+    cout<<"Kami suka mie ayam"<<endl;
+    
+}
+ int main() {
+     
+     perkenalan();
+     
+     return 0;
+ }
